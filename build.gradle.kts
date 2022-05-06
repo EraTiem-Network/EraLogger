d8f00bd67f4a166d3b793a0452df308b85e6f6da
Motzkiste <motzkiste@outlook.com>
2022-05-06 12:52:41 +0200

    Gradle Publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,11 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.6.20"
+    `maven-publish`
 }
 
 group = "net.eratiem.log"
-version = "1.0"
+version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -13,6 +14,27 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            version = project.version.toString()
+            artifactId = project.name
+
+            from(components["java"])
+        }
+    }
+    repositories {
+        maven {
+            url = uri("https://artifactory.hingel.tech/artifactory/eratiem")
+            credentials {
+                username = System.getenv("MAVEN_USER") ?: "admin"
+                password = System.getenv("MAVEN_PASS") ?: ""
+            }
+        }
+    }
 }
 
 tasks.test {
